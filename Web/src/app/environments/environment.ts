21b5cf923f74984118b4f31ccd76e9d99c4383b5
Austin Sutrick <105655554+ASutrick@users.noreply.github.com>
2023-03-29 10:26:25 -0500

    setup

---
+++ Web/src/app/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    portalApiUrl: "http://localhost:5001/api",
+    portalApiUrlBase: "https://localhost:4200",
+    redirectUrl: "http://localhost:4200"
+}
---
+++ Web/src/app/interfaces/MachineInfo.ts
@@ -0,0 +1,6 @@
+export interface MachineInfo {
+    machineId: string;
+    clientName: string;
+    machineName: string;
+    dateChecked: string;
+}
